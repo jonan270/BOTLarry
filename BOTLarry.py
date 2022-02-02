76d09e6d2dd82b29fd4301685eacbe6885e1aec7
jonan270 <57438307+jonan270@users.noreply.github.com>
2022-02-03 00:13:34 +0100

    Telephone response

--- BOTLarry.py
+++ BOTLarry.py
@@ -10,6 +10,12 @@ DISCORD_GUILD = os.getenv('DISCORD_GUILD')
 
 client = discord.Client()
 
+def getCorrectResponse(content):
+    if 'telefon' in content.lower() or 'mobil' in content.lower():
+        return "Aa fan att tekniken alltid ska strula asså!"
+    else:
+        return "" # No specific response needed
+
 
 @client.event
 async def on_ready():
@@ -26,5 +32,19 @@ async def on_ready():
     print("Active in channel: ", textChannel.name)
 
     await textChannel.send("Sorry att jag försvann lite! Måste ha råkat stoppa tån i eluttaget igen :grimacing:")
+
+@client.event
+async def on_message(message):
+    print("In")
+    # Dont talk to yourself silly Larry
+    if message.author == client.user:
+        return
+    
+    # Provide appropriate response if needed
+    response = getCorrectResponse(message.content)
+    if response != "":
+        await textChannel.send(getCorrectResponse(message.content))
+
+    
 
 client.run(DISCORD_TOKEN)

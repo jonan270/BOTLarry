fd290c9b78a71573576a3b289300a8dfdbfefd8b
jonan270 <57438307+jonan270@users.noreply.github.com>
2022-02-14 14:08:42 +0100

    Made Larry only speak in the same guild as message sender

--- BOTLarry.py
+++ BOTLarry.py
@@ -87,6 +87,7 @@ async def getCorrectResponse(content):
         return mapQuote(random.choice(mapPool))
     else:
         return "" # No specific response needed
+    
 
 @client.event
 async def on_ready():
@@ -96,13 +97,13 @@ async def on_ready():
     for guild in client.guilds:
         if guild.name == DISCORD_GUILD:
             break
-
+    
     print(f'{client.user} have joined {guild.name}!')
 
     textChannel = discord.utils.get(guild.text_channels, name="general")
     print("Active in channel: ", textChannel.name)
 
-    await textChannel.send("Sorry att jag försvann lite! Måste ha råkat stoppa tån i eluttaget igen :grimacing:")
+    #await textChannel.send("Sorry att jag försvann lite! Måste ha råkat stoppa tån i eluttaget igen :grimacing:")
 
 @client.event
 async def on_message(message):
@@ -110,11 +111,13 @@ async def on_message(message):
     # Dont talk to yourself silly Larry
     if message.author == client.user:
         return
-    
+
+    activeTextChannel = discord.utils.get(message.guild.text_channels, name="general")
+
     # Provide appropriate response if needed
     response = await getCorrectResponse(message.content)
     if response:
-        await textChannel.send(response)
+        await activeTextChannel.send(response)
     #elif isLarry(message.author.name):
     #    await textChannel.send(getCorrectResponse('!larry'))
 
